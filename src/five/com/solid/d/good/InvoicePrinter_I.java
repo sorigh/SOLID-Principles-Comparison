806f93dc49f9c2fe50749e236ee9a44f5f116add
Sorana <sorana.ghiorghe@gmail.com>
2024-10-16 00:54:46 +0300

    added dependency inversion principle

---
+++ src/five/com/solid/d/good/InvoicePrinter_I.java
@@ -0,0 +1,7 @@
+package five.com.solid.d.good;
+
+import four.com.solid.i.Invoice;
+
+public interface InvoicePrinter_I {
+    void print(Invoice invoice);
+}
--- src/four/com/solid/i/good/GoodInvoicePrinter.java
+++ src/four/com/solid/i/good/GoodInvoicePrinter.java
@@ -1,9 +1,10 @@
 package four.com.solid.i.good;
 
+import five.com.solid.d.good.InvoicePrinter_I;
 import four.com.solid.i.ComplexInvoice;
 import four.com.solid.i.Invoice;
 
-public class GoodInvoicePrinter implements GoodInvoicePrinter_I {
+public class GoodInvoicePrinter implements GoodInvoicePrinter_I, InvoicePrinter_I {
     @Override
     public void print(Invoice invoice) {
         System.out.println("Simply printing " + invoice);
